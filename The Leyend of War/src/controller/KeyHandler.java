caa8f132b0a7b1d2f04088871160748875571a0d
Anthony Alvarez Delgado <107961797+aalvarez-d@users.noreply.github.com>
2023-04-12 15:19:22 -0600

    Update KeyHandler.java

--- The Leyend of War/src/controller/KeyHandler.java
+++ The Leyend of War/src/controller/KeyHandler.java
@@ -5,6 +5,10 @@
 
 package controller;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import view.GamePanel;
+
 /**
  *
  * Program created by
@@ -12,6 +16,57 @@ package controller;
  * Software Engeneer Student - UIA
  *
  */
-public class KeyHandler {
+public class KeyHandler implements KeyListener{
+
+    
+    GamePanel gamePanel;
+    public boolean upPressed, downPressed, leftPressed, rightPressed;
+    
+    public KeyHandler(GamePanel gamePanel){
+        this.gamePanel = gamePanel;
+    }
+    
+    public void keyTyped(KeyEvent e) {
+    }
+
+    
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+        if(code==KeyEvent.VK_W || code == KeyEvent.VK_UP){
+            upPressed = true;
+        }
+        if(code==KeyEvent.VK_S || code == KeyEvent.VK_DOWN){
+            downPressed = true;
+        }
+        if(code==KeyEvent.VK_A || code == KeyEvent.VK_LEFT){
+            leftPressed = true;
+        }
+        if(code==KeyEvent.VK_D || code == KeyEvent.VK_RIGHT){
+            rightPressed = true;
+        }
+//        if(code==KeyEvent.VK_P){
+//            if(gamePanel.gameState == gamePanel.PLAY_STATE)
+//                gamePanel.gameState = gamePanel.PAUSE_STATE;
+//            else if (gamePanel.gameState == gamePanel.PAUSE_STATE)
+//                gamePanel.gameState = gamePanel.PLAY_STATE;
+//        }
+    }
+
+    
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+        if(code==KeyEvent.VK_W || code == KeyEvent.VK_UP){
+            upPressed = false;
+        }
+        if(code==KeyEvent.VK_S || code == KeyEvent.VK_DOWN){
+            downPressed = false;
+        }
+        if(code==KeyEvent.VK_A || code == KeyEvent.VK_LEFT){
+            leftPressed = false;
+        }
+        if(code==KeyEvent.VK_D || code == KeyEvent.VK_RIGHT){
+            rightPressed = false;
+        }
+    }
 
 }
