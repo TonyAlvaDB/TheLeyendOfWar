b49014da7f700f6546e7afaa9053d4c62d176d1d
Anthony Alvarez Delgado <107961797+aalvarez-d@users.noreply.github.com>
2023-04-12 19:41:31 -0600

    Update ObjectChest.java

--- The Leyend of War/src/controller/ObjectChest.java
+++ The Leyend of War/src/controller/ObjectChest.java
@@ -17,7 +17,7 @@ import javax.imageio.ImageIO;
  */
 public class ObjectChest extends SuperObject{
     
-    public void ObjectKey(){
+    public ObjectChest(){
         
         name = "Chest";
         
