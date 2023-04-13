406773eb9b742b8ad115300c76d16b664eccb141
Anthony Alvarez Delgado <107961797+aalvarez-d@users.noreply.github.com>
2023-04-12 20:42:31 -0600

    Tiles changed. Checkpoint1

--- The Leyend of War/src/controller/TileManager.java
+++ The Leyend of War/src/controller/TileManager.java
@@ -6,6 +6,7 @@
 package controller;
 
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -66,49 +67,39 @@ public class TileManager {
     }
     
     public void getTileImage(){
+
+            
+        setup(0, "tiles/dessert_floor_1.png", false);
+        setup(1, "tiles/dessert_floor_2.png", false);
+        setup(2, "tiles/dessert_floor_3.png", false);
+        setup(3, "tiles/road_1.png", false);
+        setup(4, "tiles/wall_1.png", true);
+        setup(5, "tiles/wall_2.png", true);
+        setup(6, "tiles/wall_3.png", true);
+        setup(7, "tiles/water_1.png", true);
+        setup(8, "tiles/cactu_1.png", true);
+        setup(9, "tiles/cactu_2.png", true);
+        setup(10, "tiles/cactu_3.png", true);
+        setup(11, "tiles/cactu_4.png", true);
+        setup(12, "tiles/veg_1.png", false);
+        setup(13, "tiles/veg_2.png", true);
+
+    }
+    
+    public void setup(int index, String imageName, boolean collision){
+        UtilityTool uTool = new UtilityTool();
+    
         try {
-            tile[0] = new Tile();
-            tile[0].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/dessert_floor_1.png"));
-            tile[1] = new Tile();
-            tile[1].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/dessert_floor_2.png"));
-            tile[2] = new Tile();
-            tile[2].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/dessert_floor_3.png"));
-            tile[3] = new Tile();
-            tile[3].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/road_1.png"));
-            tile[4] = new Tile();
-            tile[4].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/wall_1.png"));
-            tile[4].collision = true;
-            tile[5] = new Tile();
-            tile[5].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/wall_2.png"));
-            tile[5].collision = true;
-            tile[6] = new Tile();
-            tile[6].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/wall_3.png"));
-            tile[6].collision = true;
-            tile[7] = new Tile();
-            tile[7].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/water_1.png"));
-            tile[7].collision = true;
-            tile[8] = new Tile();
-            tile[8].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/cactu_1.png"));
-            tile[8].collision = true;
-            tile[9] = new Tile();
-            tile[9].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/cactu_2.png"));
-            tile[9].collision = true;
-            tile[10] = new Tile();
-            tile[10].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/cactu_3.png"));
-            tile[10].collision = true;
-            tile[11] = new Tile();
-            tile[11].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/cactu_4.png"));
-            tile[11].collision = true;
-            tile[12] = new Tile();
-            tile[12].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/veg_1.png"));
-            tile[13] = new Tile();
-            tile[13].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/veg_2.png"));
-            tile[13].collision = true;
-           
+            tile[index] = new Tile();
+            tile[index].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream(imageName));
+            tile[index].image = uTool.scaleImage(tile[index].image, gamePanel.TILE_SIZE, gamePanel.TILE_SIZE);
+            tile[index].collision = collision;
         } catch (IOException e) {
             e.printStackTrace();
         }
+        
     }
+    
     public void draw (Graphics2D g2){
         int worldCol = 0;
         int worldRow= 0;
@@ -127,7 +118,7 @@ public class TileManager {
                     worldX < gamePanel.player.worldX + gamePanel.player.SCREEN_X + gamePanel.TILE_SIZE*2 &&
                     worldY > gamePanel.player.worldY - gamePanel.player.SCREEN_Y - gamePanel.TILE_SIZE*2 &&
                     worldY < gamePanel.player.worldY + gamePanel.player.SCREEN_Y + gamePanel.TILE_SIZE*2){
-                g2.drawImage(tile[tileNum].image, screenX, screenY,gamePanel.TILE_SIZE, gamePanel.TILE_SIZE, null);
+                g2.drawImage(tile[tileNum].image, screenX, screenY, null);
             }
             
             
---
+++ The Leyend of War/src/controller/UtilityTool.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package controller;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * Program created by
+ * @author Anthony Alvarez Delgado
+ * Software Engeneer Student - UIA
+ *
+ */
+public class UtilityTool {
+    public BufferedImage scaleImage(BufferedImage original, int width, int hight){
+        BufferedImage scaledImage = new BufferedImage(width, hight, original.getType());
+        Graphics2D g2 = scaledImage.createGraphics();
+        g2.drawImage(original,0, 0, width, hight, null);
+        g2.dispose();
+            
+        return scaledImage;
+    }
+}
