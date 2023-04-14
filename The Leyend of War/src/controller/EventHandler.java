6a0b3160f7390365f6d2679a203c9acfccd25c6b
Anthony Alvarez Delgado <107961797+aalvarez-d@users.noreply.github.com>
2023-04-14 15:03:21 -0600

    EventHandler advanced

--- The Leyend of War/src/controller/EventHandler.java
+++ The Leyend of War/src/controller/EventHandler.java
@@ -17,48 +17,74 @@ import view.GamePanel;
  */
 public class EventHandler {
     GamePanel gamePanel;
-    Rectangle eventRect;
-    int eventRectDefaultX, eventRectDefaultY;
+    EventRect eventRect[][];
+    
+    int previousEventX, previousEventY;
+    boolean canTouchEvent = true;
+    
     
     public EventHandler(GamePanel gamePanel){
         this.gamePanel = gamePanel;
-        eventRect = new Rectangle();
-        eventRect.x = 23;
-        eventRect.y = 23;
-        eventRect.width = 2;
-        eventRect.height = 2;
-        eventRectDefaultX = eventRect.x;
-        eventRectDefaultX = eventRect.y;
+        eventRect = new EventRect[gamePanel.MAX_WORLD_COL][gamePanel.MAX_WORLD_ROW];
+        int col = 0;
+        int row = 0;
+        while(col < gamePanel.MAX_WORLD_COL && row < gamePanel.MAX_WORLD_ROW){
+            eventRect[col][row] = new EventRect();
+            eventRect[col][row].x = 23;
+            eventRect[col][row].y = 23;
+            eventRect[col][row].width = 2;
+            eventRect[col][row].height = 2;
+            eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
+            eventRect[col][row].eventRectDefaultX = eventRect[col][row].y;
+            col++;
+            if(col == gamePanel.MAX_WORLD_COL){
+                col = 0;
+                row++;
+            }
+        }
+        
     }
 
     public void checkEvent(){
-        if(hit(21,25, "up") == true){
-            damagePit(gamePanel.DIALOGUE_STATE);
+        int xDistance = Math.abs(gamePanel.player.worldX - previousEventX);
+        int yDistance = Math.abs(gamePanel.player.worldY - previousEventY);
+        int distance = Math.max(xDistance, yDistance);
+        if(distance > gamePanel.TILE_SIZE)
+            canTouchEvent = true;
+        
+        if(hit(21,25, "any") == true){
+            damagePit(21, 25, gamePanel.DIALOGUE_STATE);
         }
     }
-    public boolean hit(int eventCol, int eventRow, String reqDirection){
+    public boolean hit(int col, int row, String reqDirection){
         boolean hit = false;
         
         gamePanel.player.solidArea.x = gamePanel.player.worldX + gamePanel.player.solidArea.x;
         gamePanel.player.solidArea.y = gamePanel.player.worldY + gamePanel.player.solidArea.y;
-        eventRect.x = eventCol*gamePanel.TILE_SIZE+eventRect.x;
-        eventRect.y = eventRow*gamePanel.TILE_SIZE+eventRect.y;
+        eventRect[col][row].x = col*gamePanel.TILE_SIZE+eventRect[col][row].x;
+        eventRect[col][row].y = row*gamePanel.TILE_SIZE+eventRect[col][row].y;
         
-        if(gamePanel.player.solidArea.intersects(eventRect)){
-            if(gamePanel.player.direction.equals(reqDirection) || reqDirection.contentEquals("any"))
+        if(gamePanel.player.solidArea.intersects(eventRect[col][row]) && eventRect[col][row].eventDone==false){
+            if(gamePanel.player.direction.equals(reqDirection) || reqDirection.contentEquals("any")){
                 hit = true;
+                previousEventX = gamePanel.player.worldX;
+                previousEventY = gamePanel.player.worldY;
+            }
+            
         }
         gamePanel.player.solidArea.x = gamePanel.player.solidAreaDefaultX;
         gamePanel.player.solidArea.y = gamePanel.player.solidAreaDefaultY;
-        eventRect.x = eventRectDefaultX;
-        eventRect.y = eventRectDefaultY;
+        eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
+        eventRect[col][row].y = eventRect[col][row].eventRectDefaultY;
         
         return hit;
     }
 
-    private void damagePit(int gameState) {
+    private void damagePit(int col, int row, int gameState) {
         gamePanel.gameState = gameState;
-        gamePanel.ui.currentDialogue = "Caiste en un pozo";
+        gamePanel.ui.currentDialogue = "Caiste en un pozo \nEl pozo se lleno de tierra";
         gamePanel.player.life -= 1;
+        eventRect[col][row].eventDone = true;
+        canTouchEvent = false;
     }
 }
---
+++ The Leyend of War/src/controller/EventRect.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package controller;
+
+import java.awt.Rectangle;
+
+/**
+ *
+ * Program created by
+ * @author Anthony Alvarez Delgado
+ * Software Engeneer Student - UIA
+ *
+ */
+public class EventRect extends Rectangle{
+    int eventRectDefaultX, eventRectDefaultY;
+    boolean eventDone = false;
+}
